887fa5bdd60b7540bc4d5866192d5a5a42ff389a
KWAshen <ashen.maliaha@gmail.com>
2025-08-07 20:03:18 +0200

    primo salvatagio (copia del esercizio precedente)

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+const postsRouter = require('./routers/posts');
+
+app.use('/posts', postsRouter);
+
+app.use(express.static("immagini_e_post"));
+
+app.get('/', (req,res) =>{
+  res.send(("Benvenuti"));
+})
+
+app.listen(port,()=>{
+  console.log(`Server is running on http://localhost:${port}`);
+})
